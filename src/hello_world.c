c178c7ab26e266228d19d6f62194edb4f82c67f2
mhuss044D430 <mhuss044@gmail.com>
2015-07-10 17:19:13 -0400

    add button handler

--- src/hello_world.c
+++ src/hello_world.c
@@ -3,6 +3,24 @@
 Window *window;
 TextLayer *text_layer;
 
+up_button_pressed(ClickRecognizerRef recognizer, void *context)
+{
+}
+
+down_button_pressed(ClickRecognizerRef recognizer, void *context)
+{
+}
+select_button_pressed(ClickRecognizerRef recognizer, void *context)
+{
+}
+
+static void click_config_provider(void *context)
+{
+	window_single_click_subscribe(BUTTON_ID_UP, up_button_pressed);
+	window_single_click_subscribe(BUTTON_ID_UP, down_botton_pressed);
+	window_single_click_subscribe(BUTTON_ID_SELECT, select_button_pressed);
+}
+
 void handle_init(void) {
 	// Create a window and text layer
 	window = window_create();
@@ -15,6 +33,11 @@ void handle_init(void) {
 	
 	// Add the text layer to the window
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+
+
+	// Set up button handlers
+	window_set_click_config_provider(window, click_config_provider);
+
 
 	// Push the window
 	window_stack_push(window, true);
@@ -31,7 +54,8 @@ void handle_deinit(void) {
 	window_destroy(window);
 }
 
-int main(void) {
+int main(void)
+{
 	handle_init();
 	app_event_loop();
 	handle_deinit();
